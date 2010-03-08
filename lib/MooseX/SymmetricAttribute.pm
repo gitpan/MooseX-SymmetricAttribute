9bce2ac704ca5672b418d3a87f19edf98a4a4058
Gerhard Gossen <gerhard@cpan.org>
2010-03-08 14:33:12 +0000

    Import of GERHARD/MooseX-SymmetricAttribute-0.002 from CPAN.
    gitpan-cpan-distribution: MooseX-SymmetricAttribute
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         GERHARD/MooseX-SymmetricAttribute-0.002.tar.gz
    gitpan-cpan-author:       GERHARD
    gitpan-cpan-maturity:     released

--- lib/MooseX/SymmetricAttribute.pm
+++ lib/MooseX/SymmetricAttribute.pm
@@ -1,5 +1,5 @@
 package MooseX::SymmetricAttribute;
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 # ABSTRACT: Symmetric Moose Attributes -- automatically update inverse attribute
 use MooseX::Role::Parameterized;
 use Scalar::Util 'blessed';
@@ -47,3 +47,86 @@ sub _contains {
 
 no Moose::Role;
 1;
+
+
+
+=pod
+
+=head1 NAME
+
+MooseX::SymmetricAttribute - Symmetric Moose Attributes -- automatically update inverse attribute
+
+=head1 VERSION
+
+version 0.002
+
+=head1 Synopsis
+
+    package Foo;
+    use Moose;
+
+    has bar => (
+	is => 'rw',
+	isa => 'Bar',
+    );
+
+    with 'MooseX::SymmetricAttribute' => {
+	attributes => {
+	    bar => 'foo'
+	}
+    };
+
+    package Bar;
+    use Moose;
+
+    has foo => (
+	is => 'rw',
+	isa => 'Foo',
+    );
+
+    package main;
+    my $bar = Bar->new;
+    my $foo = Foo->new;
+
+    $foo->bar( $bar );
+
+    $bar->foo == $foo # true
+
+=head1 Usage
+
+Use this role to update both attributes in a symmetric relationship by setting
+one value.
+
+To use it, compose the role into your class like this:
+
+ with 'MooseX::SymmetricAttribute' => {
+     attributes => {
+	 my_attribute_name_1 => foreign_attribute_name_1,
+	 my_attribute_name_2 => foreign_attribute_name_2,
+     }
+ };
+
+If the foreign attribute is an ArrayRef or a Set::Object, the new value will 
+be appended.
+
+=head1 Warning
+
+This is alpha quality software. There may be some bugs lingering in here, 
+probably some race conditions too. Patches and tests are very welcome!
+
+=head1 AUTHOR
+
+  Gerhard Gossen
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Gerhard Gossen.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+__END__
+
